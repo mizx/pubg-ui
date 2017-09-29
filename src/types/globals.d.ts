863a4caaa6bee15f4b9c0b1125fee4c83211b310
Cody <codylmathisen@gmail.com>
2017-09-29 01:21:56 -0700

    added type definition to engine.call

--- src/types/globals.d.ts
+++ src/types/globals.d.ts
@@ -2,7 +2,7 @@ export interface Engine {
   on: (name: string, callback: Function, context: Object) => void;
   off: (name: string, callback: Function, context: Object) => void;
   trigger: (name: string, args?: {}) => void;
-  call: (name: string, args?: {}) => Promise<{}>;
+  call: <T>(name: string, args?: {}) => Promise<T>;
   mock: (name: string, callback: Function, isEvent: boolean) => void;
   isAttached: boolean;
 }

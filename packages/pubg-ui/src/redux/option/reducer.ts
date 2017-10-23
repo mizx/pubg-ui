cd999a2ffe351d3e16c6b1631c9c0ac2615207b6
Cody <codylmathisen@gmail.com>
2017-10-23 02:01:25 -0700

    add cases to option reducer

--- packages/pubg-ui/src/redux/option/reducer.ts
+++ packages/pubg-ui/src/redux/option/reducer.ts
@@ -18,7 +18,16 @@ export const initialState: State = {
 export default (state: State = initialState, action: Actions): State => {
   switch (action.type) {
     case ActionType.SET_REGION: {
-
+      const { region } = action.payload;
+      return { ...state, region };
+    }
+    case ActionType.SET_SQUAD_SIZE: {
+      const { squadSize } = action.payload;
+      return { ...state, squadSize };
+    }
+    case ActionType.SET_PERSPECTIVE: {
+      const { perspective } = action.payload;
+      return { ...state, perspective };
     }
     default: return state;
   }

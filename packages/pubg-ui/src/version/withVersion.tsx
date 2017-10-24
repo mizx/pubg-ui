76e2379ee91e9f4bf1bc41c97e209e1220425006
Cody <codylmathisen@gmail.com>
2017-10-23 22:04:43 -0700

    created withVersion hoc

---
+++ packages/pubg-ui/src/version/withVersion.tsx
@@ -0,0 +1,18 @@
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import { RootState } from '../redux';
+
+export interface StateProps {
+  version: string | null;
+}
+
+export type InjectedProps = StateProps;
+
+const mapStateToProps = (state: RootState): StateProps => ({
+  version: state.app.version
+});
+
+export const withVersion = <P extends {}>(Component: React.ComponentType<P & InjectedProps>) => (
+  connect(mapStateToProps)(Component)
+);

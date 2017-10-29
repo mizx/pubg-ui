6a11a7e592931f4a57cdbf8d70d3f846eea45f07
Cody <codylmathisen@gmail.com>
2017-10-29 01:36:26 -0700

    added withFriends HOC

--- packages/pubg-ui/src/index.ts
+++ packages/pubg-ui/src/index.ts
@@ -1,6 +1,7 @@
 import './coherent';
 
 export * from './button';
+export * from './friends';
 export { default as PubgProvider } from './provider';
 export * from './provider';
 export * from './options';
--- packages/pubg-ui/src/redux/friend/index.ts
+++ packages/pubg-ui/src/redux/friend/index.ts
@@ -3,7 +3,8 @@ import * as ActionType from './action-types';
 export { ActionType };
 export * from './action-creators';
 export { Actions } from './action-creators';
-export { State, default as reducer } from './reducer';
+export * from './reducer';
+export { default as reducer } from './reducer';
 export { default as epic } from './epics';
 
 import './coherent';

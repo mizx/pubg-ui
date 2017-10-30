73184f91bab8d184325c417c89d4f2b18fed04da
Cody <codylmathisen@gmail.com>
2017-10-29 23:07:36 -0700

    fixed engine.call type error

--- packages/pubg-ui/src/types/engine.ts
+++ packages/pubg-ui/src/types/engine.ts
@@ -2,7 +2,7 @@ export interface Engine {
   on: (name: string, callback: Function, context?: Object) => void;
   off: (name: string, callback: Function, context?: Object) => void;
   trigger: (name: string, ...args: any[]) => void;
-  call: <T>(name: string, args: any[]) => Promise<T>;
+  call: <T>(name: string, ...args: any[]) => Promise<T>;
   mock: (name: string, callback: Function, isEvent?: boolean) => void;
   isAttached: boolean;
 }

527139cbb902d239b8583b24fa506658e93df98b
Cody <codylmathisen@gmail.com>
2017-11-03 02:01:03 -0700

    removed obsolete auth epic/action references

--- packages/pubg-ui/src/redux/root-action.ts
+++ packages/pubg-ui/src/redux/root-action.ts
@@ -1,5 +1,4 @@
 import { Actions as AppActions } from './app';
-import { Actions as AuthActions } from './auth';
 import { Actions as FriendActions } from './friend';
 import { Actions as OptionActions } from './option';
 import { Actions as ProfileActions } from './profile';
@@ -7,7 +6,6 @@ import { Actions as SessionActions } from './session';
 
 export type RootAction =
   | AppActions
-  | AuthActions
   | FriendActions
   | OptionActions
   | ProfileActions
--- packages/pubg-ui/src/redux/root-epic.ts
+++ packages/pubg-ui/src/redux/root-epic.ts
@@ -2,7 +2,6 @@ import { combineEpics, Epic } from 'redux-observable';
 
 import { RootAction, RootState } from './index';
 import { epic as app } from './app';
-import { epic as auth } from './auth';
 import { epic as friend } from './friend';
 
 export type Epic = Epic<RootAction, RootState>;

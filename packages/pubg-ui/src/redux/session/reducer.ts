1c209d8395e0f21c67fed4d9e12b16bf0d7605e2
Cody <codylmathisen@gmail.com>
2017-11-08 00:10:07 -0800

    added country code epic and reducer case

--- packages/pubg-ui/src/redux/session/reducer.ts
+++ packages/pubg-ui/src/redux/session/reducer.ts
@@ -6,20 +6,30 @@ export interface State {
   accessToken: string | null;
   playerNetId: string | null;
   userSerial: string | null;
+  countryCode: string | null;
 }
+
+// Not sure if devs have a default countryCode
+// so going with one that will always work.
+const defaultCountryCode = 'US';
 
 export const initialState: State = {
   accessToken: null,
   playerNetId: null,
-  userSerial: null
+  userSerial: null,
+  countryCode: null
 };
 
 export default (state: State = initialState, action: Actions): State => {
   switch (action.type) {
     case ActionType.SET_ACCESS_TOKEN:
     case ActionType.SET_PLAYER_NET_ID:
-    case ActionType.SET_USER_SERIAL: {
+    case ActionType.SET_USER_SERIAL:
+    case ActionType.COUNTRY_CODE_SUCCESS: {
       return { ...state, ...action.payload };
+    }
+    case ActionType.COUNTRY_CODE_FAILURE: {
+      return { ...state, countryCode: defaultCountryCode }
     }
     default: return state;
   }

601c10c7e21dd48238d875b214bc03837053fec5
Cody <codylmathisen@gmail.com>
2017-11-13 22:16:00 -0800

    fixed tabs

--- packages/pubg-ui-theme/src/components/overlay/overlay.tsx
+++ packages/pubg-ui-theme/src/components/overlay/overlay.tsx
@@ -3,36 +3,36 @@ import React from 'react';
 import styled from 'styled';
 
 const Overlay = styled.div`
-    position: fixed;
-    width: 100vw;
-    height: 100vh;
-    z-index: 200;
-    pointer-events: none;
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  z-index: 200;
+  pointer-events: none;
 `;
 
 const OverlayLayer = styled.div`
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
 `;
 
 const OverlayHeader = OverlayLayer.extend`
-    background: url('/img/overlay_header.png') center top/100% no-repeat;
+  background: url('/img/overlay_header.png') center top/100% no-repeat;
 `;
 
 const OverlayFooter = OverlayLayer.extend`
-    background: url('/img/overlay_footer.png') center bottom/100% no-repeat;
+  background: url('/img/overlay_footer.png') center bottom/100% no-repeat;
 `;
 
 export interface Props { }
 
 export const OverlayComponent: React.SFC<Props> = props => (
-    <Overlay>
-        <OverlayHeader />
-        <OverlayFooter />
-    </Overlay>
+  <Overlay>
+    <OverlayHeader />
+    <OverlayFooter />
+  </Overlay>
 );
 
 export default OverlayComponent;

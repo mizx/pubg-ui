930d1924579de614e5738c3b7a8192f1915976dc
Cody <codylmathisen@gmail.com>
2017-11-15 01:41:15 -0800

    added closed route

--- packages/pubg-ui-theme/src/components/routes/routes.tsx
+++ packages/pubg-ui-theme/src/components/routes/routes.tsx
@@ -4,6 +4,7 @@ import styled from 'styled';
 
 import Root from './root';
 import Auth from './auth';
+import Closed from './closed';
 import Main from './main';
 import Play from './play';
 import Friends from './friends';
@@ -17,6 +18,7 @@ const Routes: React.SFC = props => (
   <Wrapper>
     <Switch>
       <Route path="/auth" component={Auth} />
+      <Route path="/closed" component={Closed} />
       <Route path="/main" component={Main} />
       <Route path="/play" component={Play} />
       <Route path="/friends" component={Friends} />

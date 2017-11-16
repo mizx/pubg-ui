97f81924faf8a8695ac29c327d67e02b90cd7ad9
Cody <codylmathisen@gmail.com>
2017-11-16 01:27:10 -0800

    added comments for basic flow

--- packages/pubg-ui/src/websocket/create.ts
+++ packages/pubg-ui/src/websocket/create.ts
@@ -6,8 +6,18 @@ import { webSocketResponse } from '../redux/app';
 
 const WEBSOCKET_URL = 'ws://echo.websocket.org';
 
+// TODO: make a queue observable to pass to socket$
+
+/**
+ * We will eventually need this to start after WEBSOCKET_INIT action is called
+ */
 export const socket$ = webSocket(WEBSOCKET_URL);
 
+/**
+ * the 1st callback will eventually need to go to a switch statement
+ * of some kind. We'll also probably have to keep an incrementing
+ * counter to map responses back to their request.
+ */
 socket$.subscribe(
   (msg) => store.dispatch(webSocketResponse(msg as number)),
   (err) => console.error('error:', err),
--- packages/pubg-ui/src/websocket/middleware.ts
+++ packages/pubg-ui/src/websocket/middleware.ts
@@ -1,11 +1,19 @@
 import { Middleware } from 'redux';
 
+import { ActionType } from '../redux/app';
 import { socket$ } from '../websocket/create';
 
+/**
+ * Watch for websocket requests being dispatched.
+ * When a call gets dispatched, pull state using
+ * `api.getState()` and use a redux selector to
+ * get whatever state data you need for the 
+ * websocket request.
+ */
 export function webSocketMiddleware(): Middleware {
-  return (api) => (next) => action => {
+  return api => next => action => {
     switch (action.type) {
-      case 'APP::WEBSOCKET_REQUEST': {
+      case ActionType.WEBSOCKET_REQUEST: {
         socket$.next(Math.random());
       }
     }

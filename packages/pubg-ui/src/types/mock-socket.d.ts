06a209a6089607b1118b2079e9bda1b9ac2762e5
Cody <codylmathisen@gmail.com>
2017-11-19 15:09:19 -0800

    added mock-socket type definition

--- packages/pubg-ui/src/types/mock-socket.d.ts
+++ packages/pubg-ui/src/types/mock-socket.d.ts
@@ -1,5 +1,11 @@
+// Type definitions for mock-socket 7.1.0
+// Project: https://github.com/thoov/mock-socket
+// Definitions by: mizx https://github.com/mizx
+
+/// <reference types="socket.io" />
+
 declare module 'mock-socket' {
     export var WebSocket: WebSocket;
-    export var Server: any;
-    export var SocketIO: any;
+    export var Server: SocketIO.Server;
+    export var SocketIO: SocketIOStatic;
 }
